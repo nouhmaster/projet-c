b92954d8c9cc359baf7f9d72cd4b5e528690dfdf
Zineddine Berrichi <33991627+azetoy@users.noreply.github.com>
2020-04-11 18:50:36 +0200

    Update test.c
    implémentation des déplacement de la case 0 et de la structure permettant de sauvegarder la position actuelle de la case 0

--- test.c
+++ test.c
@@ -3,33 +3,66 @@
 #include <unistd.h>
 #include <time.h>
 
+typedef struct Pos
+{
+  int x;
+  int y;
+}Pos;
+
 void fill_2darray(int **A, int L, int C);
 int ** create_2darray(int L, int C);
 void create_2darray_bis(int ***A, int L, int C);
 void disp_2darray(int **A,int L,int C);
 void freeA(int ** A);
+void droite(int **A,int L,int C,struct Pos *pos);
+void gauche(int **A,int L,int C,struct Pos *pos);
+void bas(int **A,int L,int C,struct Pos *pos);
+void haut(int **A,int L,int C,struct Pos *pos);
 
 int main()
 {
-  int x = 0;
-  int y = 0;
+  int l = 4;
+  int c = 4;
+  Pos *pos;
+  pos = (Pos *) malloc(sizeof(Pos));
+
+  pos->x = l-1;
+  pos->y = c-1;
+
   int **tab = NULL;
-  printf("entré le nombre de ligne que vous voulez : ");
-  scanf("%d",&x);
-  printf("entré le nombre de colonne que vous voulez : ");
-  scanf("%d",&y);
-  create_2darray_bis(&tab,x,y);
-  fill_2darray(tab,x,y);
-  disp_2darray(tab,x,y);
-  freeA(tab);
+
+  create_2darray_bis(&tab,l,c);
+  fill_2darray(tab,l,c);
+  disp_2darray(tab,l,c);
+  haut(tab,pos->x,pos->y,pos);
+disp_2darray(tab,l,c);
+haut(tab,pos->x,pos->y,pos);
+disp_2darray(tab,l,c);
+haut(tab,pos->x,pos->y,pos);
+disp_2darray(tab,l,c);
+bas(tab,pos->x,pos->y,pos);
+disp_2darray(tab,l,c);
+bas(tab,pos->x,pos->y,pos);
+disp_2darray(tab,l,c);
+gauche(tab,pos->x,pos->y,pos);
+disp_2darray(tab,l,c);
+droite(tab,pos->x,pos->y,pos);
+disp_2darray(tab,l,c);
+
+
+
+
+
+
+  //direction(tab,x-1,y-1);
+
+  //freeA(tab);
 }
+
 
 
 void create_2darray_bis(int ***A, int L, int C) // on cree un tableaux a 2 dimension qui va contenir le "plateaux de jeux"
 {
-
-    if(*A==NULL) // on verifie que l'allocation memoire a bien reussie
-        printf("Erreur malloc ");
 
   *A = (int **)malloc(sizeof(int*) * L); // allocation memoire dynamique en fonction du nombre de ligne
     for (int i = 0; i < L; i++)
@@ -40,11 +73,9 @@ void create_2darray_bis(int ***A, int L, int C) // on cree un tableaux a 2 dimen
 
 void fill_2darray(int **A, int L, int C) // fonction qui remplit le plateaux de nombre aleatoire comprit entre 15 et 0
 {
-  int count_j;
-  int count_i;
+
   for (int i = 0; i <  L; i++)
     {
-      usleep(5000);
         for (int j = 0; j < C; j++)
         {
 
@@ -57,7 +88,7 @@ void fill_2darray(int **A, int L, int C) // fonction qui remplit le plateaux de 
                         A[i][j] = 0; // permet de remplacer la derniere case du tableaux par 0 pour pouvoir jouer
                     }
                     else
-                        A[i][j]=(rand()%15)+1;// remplit la case du tableaux avec un nombre aleatoire generer par rand entre 15 et 0
+                        A[i][j]=rand()%(15 + 1 - 1) + 1;// remplit la case du tableaux avec un nombre aleatoire generer par rand entre 15 et 0
                 }
 
         }
@@ -74,9 +105,41 @@ void disp_2darray(int **A,int L,int C)// affiche le contenus du tableaux
         }
         printf("\n");
     }
+    printf("\n");
+    printf("\n");
+    //direction(A,L,C);
 }
 
 void freeA(int **A) // libere l'espace allouer au tableaux
 {
     free(A); // permet de librere l'espace memoire du tableaux a la fin du programme
+}
+
+void haut(int **A,int L,int C,struct Pos *pos)
+{
+    int temps; // 1 2 3
+    temps = A[pos->y][pos->x]; //2
+    A[pos->y][pos->x] = A[--(pos->y)][pos->x]; // 2 -> 1
+    A[pos->y][pos->x] = temps;
+}
+void droite(int **A,int L,int C,struct Pos *pos)
+{
+    int temps; // 1 2 3
+    temps = A[pos->y][pos->x]; //2
+    A[pos->y][pos->x] = A[pos->y][++(pos->x)]; // 2 -> 1
+    A[pos->y][pos->x] = temps;
+}
+void gauche(int **A,int L,int C,struct Pos *pos)
+{
+    int temps; // 1 2 3
+    temps = A[pos->y][pos->x]; //2
+    A[pos->y][pos->x] = A[pos->y][--(pos->x)]; // 2 -> 1
+    A[pos->y][pos->x] = temps;
+}
+void bas(int **A,int L,int C,struct Pos *pos)
+{
+    int temps; // 1 2 3
+    temps = A[pos->y][pos->x]; //2
+    A[pos->y][pos->x] = A[++(pos->y)][pos->x]; // 2 -> 1
+    A[pos->y][pos->x] = temps;
 }
